babb8e6c67b4aa8130ab6064e469fecd38493bf7
iltramont <luca.tramonti@outlook.it>
2025-08-20 01:06:12 +0200

    modificato file di prova

--- src/prova.py
+++ src/prova.py
@@ -1,1 +1,7 @@
-print('Hello world')
+print('Una simpatica e curiosa funzione')
+
+def connesioni(n):
+    """restituisce il numero di connessioni generate da n elementi"""
+    return 0.5 * n * (n-1)
+
+print(connesioni(7))
