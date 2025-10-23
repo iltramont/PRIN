8a3d9317a2b4b01678da8684ef95e7926a3f13aa
Luca Tramonti <luca.tramonti@outlook.it>
2025-10-24 01:01:57 +0200

    splitting

--- utils/pulisci_splitta_dati.py
+++ utils/pulisci_splitta_dati.py
@@ -3,15 +3,26 @@ import os
 import numpy as np
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+import random
 
 
 TEST_SIZE = 0.2
 VALIDATION_SIZE = 0.05
 RANDOM_STATE = 2025
 STRATIFY_COLUMNS = (
-    'morfologia',
-    'infiltrazione_sfinteri'
+    'posizione',
+#    'infiltrazione_sfinteri',
+#    'infiltrazione_tessuto_adiposo',
+#    'depositi_tumorali',
+#    'emvi_esteso',
+#    'stadio_T',
+#    'stadio_N',
+#    'stadio_N1c',
+#    'metastasi'
 )
+
+random.seed(RANDOM_STATE)
+
 
 base_dir = Path(__file__).parent.parent
 print(base_dir)
@@ -35,51 +46,76 @@ data_clean.reset_index(inplace=True, drop=True)
 
 print(f'{data_clean.shape = }')
 
-
 # Isola righe che hanno valori di colonne appartenenti a classi poco numerose (minori di 2)
-lesioni_ossee_si = data_clean[data_clean.lesioni_ossee == 'si']
-"""
-infiltrazione_sfinteri_interno = data_clean[data_clean.infiltrazione_sfinteri == 'interno']
-infiltrazione_sfinteri_interno_piano_esterno = data_clean[data_clean.infiltrazione_sfinteri == 'interno_piano_esterno']
-coinvolgimento_riflessione_peritoneale_rischio = data_clean[data_clean.coinvolgimento_riflessione_peritoneale == 'rischio']
-coinvolgimento_riflessione_peritoneale_NAN = data_clean[data_clean.coinvolgimento_riflessione_peritoneale.fillna('NaN') == 'NaN']
-coinvolgimento_fascia_mesorettale_NaN = data_clean[data_clean.coinvolgimento_fascia_mesorettale.fillna('NaN') == 'NaN']
-depositi_tumorali_sospetto = data_clean[data_clean.depositi_tumorali == 'sospetto']
-mrf_NaN = data_clean[data_clean.mrf.fillna('NaN') == 'NaN']
-stadio_N_NaN = data_clean[data_clean.stadio_N.fillna('NaN') == 'NaN']
-stadio_N_N1c = data_clean[data_clean.stadio_N == 'N1c']
-"""
-print(len(lesioni_ossee_si))
-"""
-print(len(infiltrazione_sfinteri_interno))
-print(len(infiltrazione_sfinteri_interno_piano_esterno))
-print(len(coinvolgimento_riflessione_peritoneale_rischio))
-print(len(coinvolgimento_riflessione_peritoneale_NAN))
-print(len(coinvolgimento_fascia_mesorettale_NaN))
-print(len(depositi_tumorali_sospetto))
-print(len(mrf_NaN))
-print(len(stadio_N_NaN))
-print(len(stadio_N_N1c))"""
-print(lesioni_ossee_si.index)
-"""
-print(infiltrazione_sfinteri_interno.index)
-print(infiltrazione_sfinteri_interno_piano_esterno.index)
-print(coinvolgimento_riflessione_peritoneale_rischio.index)
-print(coinvolgimento_riflessione_peritoneale_NAN.index)
-print(coinvolgimento_fascia_mesorettale_NaN.index)
-print(depositi_tumorali_sospetto.index)
-print(mrf_NaN.index)
-print(stadio_N_NaN.index)
-print(stadio_N_N1c.index)"""
 
-# Crea split di dati
-"""train_split, test_split = train_test_split(data_clean,
-                                           test_size=TEST_SIZE,
-                                           random_state=RANDOM_STATE,
-                                           stratify=data_clean[list(STRATIFY_COLUMNS)].fillna('NaN')
-                                           )
+colonne = [
+    'lesioni_ossee',
+    'infiltrazione_sfinteri',
+    'infiltrazione_sfinteri',
+    'coinvolgimento_riflessione_peritoneale',
+    'coinvolgimento_riflessione_peritoneale',
+    'coinvolgimento_fascia_mesorettale',
+    'depositi_tumorali',
+    'stadio_N',
+    'stadio_N',
+    'mrf',
+    'infiltrazione_tessuto_adiposo'
+]
+valori = [
+    'si',
+    'interno',
+    'interno_piano_esterno',
+    'rischio',
+    'NaN',
+    'NaN',
+    'sospetto',
+    'N1c',
+    'NaN',
+    'NaN',
+    'NaN'
+]
 
-print(train_split.morfologia.value_counts())
-print(test_split.morfologia.value_counts())
-"""
-print(data_clean.iloc[127].mrf is np.nan)
+train_rows = []
+test_rows = []
+for col, val in zip(colonne, valori):
+    if val == 'NaN':
+        index = data_clean[data_clean[col].fillna('NaN') == val].index
+    else:
+        index = data_clean[data_clean[col] == val].index
+    if len(index) == 0:
+        continue
+    elif len(index) == 1:
+        train_rows.append(data_clean.iloc[index])
+        # tolgo riga dal dataset
+        data_clean.drop(index=index, inplace=True)
+    elif len(index) == 2:
+        x = random.randint(0, 1)
+        y = 1 - x
+        train_rows.append(data_clean.iloc[index[x]].to_frame().T)
+        test_rows.append(data_clean.iloc[index[y]].to_frame().T)
+        # Tolgo riga dal dataset
+        data_clean.drop(index=index, inplace=True)
+    else:
+        continue
+    
+print('OK')
+print(f'{data_clean.shape = }')
+
+# Crea gli split
+train_split = pd.concat(train_rows)
+test_split = pd.concat(test_rows)
+
+# Splitta i dati rimasti
+for col in STRATIFY_COLUMNS:
+    print(data_clean.fillna('NaN')[col].value_counts())
+    print("\n")
+    
+train, test = train_test_split(data_clean,
+                               test_size=TEST_SIZE,
+                               random_state=RANDOM_STATE,
+                               stratify=data_clean[list(STRATIFY_COLUMNS)].fillna('NaN'))
+
+
+
+print(f'{train.shape = }')
+print(f'{test.shape = }')
