660d113e98f99f29ac9bb026d78f252f89bca891
Luca Tramonti <luca.tramonti@outlook.it>
2025-10-26 16:13:54 +0100

    Refactor data processing to use iterative_train_test_split and clean up code structure

--- utils/pulisci_splitta_dati.py
+++ utils/pulisci_splitta_dati.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os
 import numpy as np
 from pathlib import Path
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
+from skmultilearn.model_selection import iterative_train_test_split
 import random
+from schema_json import ReportData
 
 
 TEST_SIZE = 0.2
@@ -11,14 +13,14 @@ VALIDATION_SIZE = 0.05
 RANDOM_STATE = 2025
 STRATIFY_COLUMNS = (
     'posizione',
-#    'infiltrazione_sfinteri',
-#    'infiltrazione_tessuto_adiposo',
-#    'depositi_tumorali',
-#    'emvi_esteso',
-#    'stadio_T',
-#    'stadio_N',
-#    'stadio_N1c',
-#    'metastasi'
+    'infiltrazione_sfinteri',
+    'infiltrazione_tessuto_adiposo',
+    'depositi_tumorali',
+    'emvi_esteso',
+    'stadio_T',
+    'stadio_N',
+    'stadio_N1c',
+    'metastasi'
 )
 
 random.seed(RANDOM_STATE)
@@ -32,21 +34,27 @@ data_path = base_dir / "data" / "base.tumoreprimitivo.csv"
 # Se il file non è presente, inserirlo manualmente prendendolo dalla cartella dropbox
 data = pd.read_csv(data_path)
 
+
 # Elimina righe doppie
 # Intere righe duplicate. Keep last perchè i report hanno id decrescente
 duplicati = data.iloc[:, 1:].duplicated(keep='last')
 data_clean = data[duplicati == False]
-
 # Rimuovi righe con report duplicati
 duplicati = data_clean['report_text'].duplicated(keep='last')
 data_clean = data_clean[duplicati == False]
-
 # Resetta indici
 data_clean.reset_index(inplace=True, drop=True)
 
 print(f'{data_clean.shape = }')
 
-# Isola righe che hanno valori di colonne appartenenti a classi poco numerose (minori di 2)
+
+# Keep omly report and target columns
+target_columns = ReportData.model_fields.keys()
+X = data_clean[['report_text'] + list(target_columns)].copy(deep=True)
+print(f'{X.shape = }')
+
+
+# Isola righe che hanno features appartenenti a classi poco numerose (minori di 2)
 
 colonne = [
     'lesioni_ossee',
@@ -79,31 +87,50 @@ train_rows = []
 test_rows = []
 for col, val in zip(colonne, valori):
     if val == 'NaN':
-        index = data_clean[data_clean[col].fillna('NaN') == val].index
+        index = X[X[col].fillna('NaN') == val].index
     else:
-        index = data_clean[data_clean[col] == val].index
+        index = X[X[col] == val].index
     if len(index) == 0:
         continue
     elif len(index) == 1:
-        train_rows.append(data_clean.iloc[index])
+        train_rows.append(X.iloc[index])
         # tolgo riga dal dataset
-        data_clean.drop(index=index, inplace=True)
+        X.drop(index=index, inplace=True)
     elif len(index) == 2:
         x = random.randint(0, 1)
         y = 1 - x
-        train_rows.append(data_clean.iloc[index[x]].to_frame().T)
-        test_rows.append(data_clean.iloc[index[y]].to_frame().T)
+        train_rows.append(X.iloc[index[x]].to_frame().T)
+        test_rows.append(X.iloc[index[y]].to_frame().T)
         # Tolgo riga dal dataset
-        data_clean.drop(index=index, inplace=True)
+        X.drop(index=index, inplace=True)
     else:
         continue
     
 print('OK')
-print(f'{data_clean.shape = }')
+print(f'{X.shape = }')
 
 # Crea gli split
 train_split = pd.concat(train_rows)
 test_split = pd.concat(test_rows)
+
+print(f'{train_split.shape = }')
+print(f'{test_split.shape = }')
+
+Y = X.drop(columns='report_text').copy(deep=True)
+X = X['report_text'].copy(deep=True)
+
+print(f'{X.shape = }')
+print(f'{Y.shape = }')
+
+# Selezioniamo solo le colonne su cui vogliamo stratificare
+X_strat = X.values
+Y_strat = Y[list(STRATIFY_COLUMNS)].values
+
+X_train, Y_train, X_test, Y_test = iterative_train_test_split(X_strat, Y_strat, test_size=TEST_SIZE)
+
+
+
+exit()
 
 # Splitta i dati rimasti
 for col in STRATIFY_COLUMNS:
