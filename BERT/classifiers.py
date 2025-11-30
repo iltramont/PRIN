96a3b18ade3a54d39416fca2b5ae0cf64ba7d1c9
iltramont <luca.tramonti@outlook.it>
2025-11-30 22:14:15 +0100

    train BERT with pytorch

--- BERT/classifiers.py
+++ BERT/classifiers.py
@@ -1,21 +1,13 @@
-import sys
-import os
-from dotenv import load_dotenv
-from pathlib import Path
-
-import json
-from utils.schema_json import ReportData, AnnotatedReport
-import time
-from IPython.display import clear_output
-
-from huggingface_hub import login
-
 import torch
 import torch.nn as nn
+import sys
+from pathlib import Path
+from transformers import AutoModel
 
-from transformers import AutoTokenizer, AutoModel
-from datasets import load_dataset, Dataset, DatasetDict
+project_root = Path.cwd().resolve().parent
+sys.path.insert(0, str(project_root))
 
+from utils.schema_json import *
 
 
 class SimpleExtractor(nn.Module):
@@ -32,6 +24,33 @@ class SimpleExtractor(nn.Module):
 
     def forward(self, input_ids, attention_mask):
         out = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+
+        # Better than pooler_output
+        cls = out.last_hidden_state[:, 0, :]
+        # Alternative:
+        # cls = out.pooler_output
+        cls = self.dropout(cls)
+
+        return {
+            "morfologia": self.class_morfologia(cls),
+            "tessuto_adiposo": self.class_tessuto_adiposo(cls),
+        }
+
+
+class ReportExtractor(nn.Module):
+    def __init__(self, checkpoint="bert-base-multilingual-cased", num_classes_morfologia=4, num_classes_tessuto_adiposo=5):
+        super().__init__()
+        self.checkpoint = checkpoint
+        self.encoder = AutoModel.from_pretrained(checkpoint)
+        hidden = self.encoder.config.hidden_size  # Embedding length
+        self.dropout = nn.Dropout(0.2)
+
+        # Classification heads
+        self.class_morfologia = nn.Linear(hidden, num_classes_morfologia)
+        self.class_tessuto_adiposo = nn.Linear(hidden, num_classes_tessuto_adiposo)
+
+    def forward(self, input_ids, attention_mask):
+        out = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
 
         # Better than pooler_output
         cls = out.last_hidden_state[:, 0, :]
