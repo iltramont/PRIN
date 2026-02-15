84543acc51f81be34a2965db9c61f0ad08e8fbd6
Luca Tramonti <luca.tramonti@outlook.it>
2026-02-15 19:59:54 +0100

    scores

--- MAIN/performance.py
+++ MAIN/performance.py
@@ -26,7 +26,20 @@ from sklearn.metrics import jaccard_score
 ###########
 base_dir = Path(__file__).parent.parent
 ANN_MODEL = constants.RectalCancerStagingData
-RESULTS_FILE = "results_opus-4.6.jsonl"
+
+RESULT_FILES = (
+    'results_gpt-4.1-nano.jsonl',
+    'results_gpt-4.1-nano-tuned.jsonl',
+    'results_gpt-4.1-nano-tuned-oversampling.jsonl',
+    'results_gpt-4.1-mini.jsonl',
+    'results_gpt-4.1.jsonl',
+    'results_gpt-4.1-tuned-oversampling.jsonl',
+    'results_gpt-5-nano.jsonl',
+    'results_gpt-5.2.jsonl',
+    'results_mistral_large_3.jsonl',
+    'results_opus-4.6.jsonl'
+)
+
 # Set plot style
 plt.style.use('ggplot')
 #sns.set_palette('hls')
@@ -96,7 +109,6 @@ def infiltrazione_tessuto_adiposo_score(actual, prediction) -> float:
     else:
         return 0.0
 
-
 def compare_prediction(actual: ANN_MODEL, prediction: ANN_MODEL) -> pd.DataFrame:
     reg_fields = model_utils.get_regression_fields(ANN_MODEL)
     multilabel_fields = model_utils.get_multiple_choice_fields(ANN_MODEL)
@@ -111,7 +123,6 @@ def compare_prediction(actual: ANN_MODEL, prediction: ANN_MODEL) -> pd.DataFrame
         elif f == "stadio_T":
             row = [f, a, p, stadio_t_score(a, p)]
         elif f == "infiltrazione_tessuto_adiposo":
-            print(a, p)
             row = [f, a, p, infiltrazione_tessuto_adiposo_score(a, p)]
         elif f in multilabel_fields:
             row = [
@@ -125,45 +136,39 @@ def compare_prediction(actual: ANN_MODEL, prediction: ANN_MODEL) -> pd.DataFrame
                 row.append(1.0)
             else:
                 row.append(jaccard_score(abits, pbits))
-            print(row)
         else:
             row = [f, a, p, float(int(a == p))]
         rows.append(row)
+    return pd.DataFrame.from_records(rows, columns=['field', 'actual', 'perdicted', 'score'])
+
+def prediction_score(actual: ANN_MODEL, prediction: ANN_MODEL) -> float:
+    comp_df = compare_prediction(actual, prediction)
+    return comp_df['score'].mean()
 
 
+result_dfs = []
+for f in RESULT_FILES:
+    data = load_results_data(f)
+    scores = []
+    for r in data:
+        row = [
+            r['id'],
+            r['split'],
+            r['model'],
+            prediction_score(r['actual'], r['prediction'])
+        ]
+        scores.append(row)
+    df = pd.DataFrame.from_records(scores, columns=['id', 'split', 'model', 'score'])
+    result_dfs.append(df)
 
-    # Ore inizio
-    # Ore fine
-    # Spessore parietale
-    # Estensione cranio caudale
-    # Distanza OAI
-    # Posizione
-    # Riflessione peritoneale anteriore
-    # Infiltrazione tessuto adiposo
-    # Infiltrazione sfinteri
-    # Infiltrazione organi extra
-    # Infiltrazione organi dettagli
-    # Coinvolgimento riflessione peritoneale
-    # Coinvolgimento fascia mesorettale
-    # Numero linfonodi non conosciuto
-    # Linfonodi sospetti
-    # Sedi linfonodi
-    # Depositi tumorali
-    # EMVI
-    # Stadio T
-    # Stadio N
-    # Stadio N1c
-    # MRF
-    # Metastasi
+result_df: pd.DataFrame = pd.concat(result_dfs)
+    
+result_df = result_df.pivot(index=['id', 'split'], columns='model', values='score')
+result_df.sort_values(by=['split', 'id'], ascending=[False, True], inplace=True)
 
+result_df.to_csv(base_dir/"data"/"metrics"/"scores.csv")
 
-
-
-
-data = load_results_data(RESULTS_FILE)
-
-compare_prediction(data[0]['actual'], data[0]['prediction'])
-
+print(result_df)
 
 
 
